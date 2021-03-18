61f449cc4c61229b440962919a39bc3953eb4f71
Nishan <arifuzzamannishan@gmail.com>
2021-03-19 00:46:21 +0600

    initialized

---
+++ src/Component/Login/Login.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import './Login.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.confiq';
+import './Login.css';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+const Login = () => {
+    // const handleFb = () => {
+    //     const fbProvider = new firebase.auth.FacebookAuthProvider();
+
+    //     firebase
+    //         .auth()
+    //         .signInWithPopup(fbProvider)
+    //         .then((result) => {
+    //             var credential = result.credential;
+
+    //             // The signed-in user info.
+    //             var user = result.user;
+    //             console.log(user);
+
+    //         })
+    //         .catch((error) => {
+    //             // Handle Errors here.
+    //             var errorCode = error.code;
+    //             var errorMessage = error.message;
+    //             // The email of the user's account used.
+    //             var email = error.email;
+    //             // The firebase.auth.AuthCredential type that was used.
+    //             var credential = error.credential;
+    //             console.log(errorCode, errorMessage, email, credential);
+
+    //             // ...
+    //         });
+    // }
+
+    return (
+        <>
+            <div className="container d-flex justify-content-center">
+                <div className="card mt-5" style={{ width: "18rem" }}>
+                    <div style={{ border: 'none' }} className='card-header'>
+                        <h2 className='text-center'>Login</h2>
+                    </div>
+                    <div className="card-body">
+                        <form onClick=''>
+                            <input className='form-control mb-4' required type="email" name='email' placeholder='Your email' />
+                            <input className='form-control mb-4' type="password" name='password' placeholder='Your password' />
+                            <input className='form-control bg-danger logged-in' type="submit" value='LogIn' />
+                        </form>
+                        <div>
+                            <small>
+                                <p className='mt-5 text-center'>Or Sign Up Using</p>
+                            </small>
+                            <div className='d-flex justify-content-around mt-4'>
+                                <div className='icon facebook'>
+                                    <FontAwesomeIcon icon={faFacebookF} />
+                                </div>
+                                <div className='icon google'>
+                                    <FontAwesomeIcon icon={faGoogle} />
+                                </div>
+
+                                <div className='icon twitter'>
+                                    <FontAwesomeIcon icon={faTwitter} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Login;
---
+++ src/Component/Login/firebase.confiq.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAC3Q1qTxf7Q5SupNvxSTvCMnwMx-HAFic",
+    authDomain: "we-mover.firebaseapp.com",
+    projectId: "we-mover",
+    storageBucket: "we-mover.appspot.com",
+    messagingSenderId: "301735911526",
+    appId: "1:301735911526:web:5444283c61d65f8eb2cda7"
+  };
+  export default firebaseConfig;
