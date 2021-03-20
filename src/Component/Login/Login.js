5f14d78223b750ed56b585b07f3caca96a12a907
Nishan <arifuzzamannishan@gmail.com>
2021-03-20 19:48:28 +0600

    bug fixed

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -21,7 +21,7 @@ if (!firebase.apps.length) {
 const Login = () => {
     const { register, handleSubmit, watch, errors, reset } = useForm();
     const password = useRef({});
-    // password.current = watch("password", "");
+    password.current = watch("password", "");
 
 
     const [loggedInUser, setLoggedInUser] = useContext(userContext);
@@ -75,6 +75,7 @@ const Login = () => {
                     console.log(errorCode, errorMessage);
                 });
         }
+        // reset();
     }
 
     console.log(loggedInUser);
@@ -110,8 +111,8 @@ const Login = () => {
 
 
 
-    const { name } = useParams();
-    const vehicleInfo = fakeData.find(vehicleName => vehicleName.name === name);
+    // const { name } = useParams();
+    // const vehicleInfo = fakeData.find(vehicleName => vehicleName.name === name);
 
 
     const [newUser, setNewUser] = useState(false);
@@ -121,114 +122,80 @@ const Login = () => {
     }
 
 
-    // const handleFb = () => {
-    //     const fbProvider = new firebase.auth.FacebookAuthProvider();
-
-    //     firebase
-    //         .auth()
-    //         .signInWithPopup(fbProvider)
-    //         .then((result) => {
-    //             var credential = result.credential;
-
-    //             // The signed-in user info.
-    //             var user = result.user;
-    //             console.log(user);
-
-    //         })
-    //         .catch((error) => {
-    //             // Handle Errors here.
-    //             var errorCode = error.code;
-    //             var errorMessage = error.message;
-    //             // The email of the user's account used.
-    //             var email = error.email;
-    //             // The firebase.auth.AuthCredential type that was used.
-    //             var credential = error.credential;
-    //             console.log(errorCode, errorMessage, email, credential);
-
-    //             // ...
-    //         });
-    // }
-
     return (
         <>
             <div className="container d-flex justify-content-center">
-                <div className="card mt-5" style={{ width: "18rem" }}>
-                    <div style={{ border: 'none' }} className='card-header'>
-                        <h2 className='text-center'>Login</h2>
-                    </div>
-                    <div className="card-body">
-                        <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="card pl-5 pr-5 pt-5 pb-3 mt-2" style={{ width: "23rem" }}>
+                    {
+                        newUser ? <h2 className='text-center text-primary'>Sign Up</h2>
+                            :
+                            <h2 className='text-center text-primary'>Login</h2>
+                    }
+                    <form onSubmit={handleSubmit(onSubmit)}>
 
-                            {/* for name */}
-                            {newUser && <input className='form-control' type="name" name='name' placeholder='Your name' ref={register({ required: true })} />}
-                            {errors.name && <span className='text-danger'>name field is required</span>}
+                        {/* for name */}
+                        {newUser && <input className='mt-3 form-control' type="name" name='name' placeholder='Your name' ref={register({ required: true })} />}
+                        {errors.name && <span className='text-danger'>name field is required</span>}
 
-                            {/* for email */}
-                            <input className='mt-4 form-control' type="email" name='email' placeholder='Your email' ref={register({ required: true })} />
-                            {errors.email && <span className='text-danger'>Email field is required</span>}
+                        {/* for email */}
+                        <input className='mt-3 form-control' type="email" name='email' placeholder='Your email' ref={register({ required: true })} />
+                        {errors.email && <span className='text-danger'>Email field is required</span>}
 
-                            {/* for password */}
-                            <input className='mt-4 form-control' type="password" name='password' placeholder='Your password' ref={register({
-                                required: "You must specify a password",
-                                pattern: /[a-zA-Z][0-9]/,
-                                minLength: {
-                                    value: 8,
-                                    message: "Password must have at least 8 characters"
-                                }
-                            })} />
-                            {errors.password && <span className='text-danger'>{errors.password.message || 'password should contain at least 1 letter and 1 number'}</span>}
-
-                            {/* for confirm password */}
-                            {newUser && <input className='mt-4 form-control' type="password" name='confirmPassword' placeholder='Confirm password' ref={register({
-                                validate: value =>
-                                    value === password.current || "The passwords do not match"
-                            })} />}
-                            {
-                                errors.confirmPassword && <span className='text-danger'>{errors.confirmPassword.message}</span>
+                        {/* for password */}
+                        <input className='mt-3 form-control' type="password" name='password' placeholder='Your password' ref={register({
+                            required: "You must specify a password",
+                            pattern: /[a-zA-Z][0-9]/,
+                            minLength: {
+                                value: 8,
+                                message: "Password must have at least 8 characters"
                             }
+                        })} />
+                        {errors.password && <span className='text-danger'>{errors.password.message || 'password should contain at least 1 letter and 1 number'}</span>}
 
-                            {/* for submit button */}
-                            {
-                                newUser ? <input className='mt-4 form-control bg-danger logged-in text-white' type="submit" value='Create an account' /> : <input className='mt-4 form-control bg-danger logged-in text-white' type="submit" value='Log In' />
-                            }
-                        </form>
-                        <div>
-                            <small>
-                                <p className='mt-5 text-center'>Or Sign Up Using</p>
-                            </small>
+                        {/* for confirm password */}
+                        {newUser && <input className='mt-3 form-control' type="password" name='confirmPassword' placeholder='Confirm password' ref={register({
+                            validate: value =>
+                                value === password.current || "The passwords do not match"
+                        })} />}
+                        {
+                            errors.confirmPassword && <span className='text-danger'>{errors.confirmPassword.message}</span>
+                        }
 
-                            {/* for icons */}
-                            <div className='d-flex justify-content-around mt-4'>
-                                <div className='icon facebook'>
-                                    <FontAwesomeIcon icon={faFacebookF} />
-                                </div>
-                                <div onClick={handleGoogleSignIn} className='icon google'>
-                                    <FontAwesomeIcon icon={faGoogle} />
-                                </div>
+                        {/* for submit button */}
+                        {
+                            newUser ? <input className='mt-3 btn bg-danger form-control text-white' type="submit" value='Create an account' /> : <input className='mt-4 btn btn-danger form-control text-white' type="submit" value='Log In' />
+                        }
+                    </form>
+                    <div>
+                        <small>
+                            <p className='mt-3 text-center'>Or Sign Up Using</p>
+                        </small>
 
-                                <div className='icon twitter'>
-                                    <FontAwesomeIcon icon={faTwitter} />
-                                </div>
+                        {/* for icons */}
+                        <div className='d-flex justify-content-around mt-3'>
+                            <div onClick={handleGoogleSignIn} className='icon google'>
+                                <FontAwesomeIcon icon={faGoogle} />
                             </div>
 
-                            {/* for last text */}
-                            {
-                                newUser ?
-                                    <div>
-                                        <small>
-                                            <p className='mt-5 text-center'>Already have an account</p>
-                                        </small>
-                                        <p onClick={handleNewUser} className='text-center'>Log In</p>
-                                    </div>
-                                    :
-                                    <div>
-                                        <small>
-                                            <p className='mt-5 text-center'>Don't have an account</p>
-                                        </small>
-                                        <p onClick={handleNewUser} className='text-center'>Sign Up</p>
-                                    </div>
-                            }
                         </div>
+
+                        {/* for last text */}
+                        {
+                            newUser ?
+                                <div>
+                                    <small>
+                                        <p className='mt-3 text-center'>Already have an account</p>
+                                    </small>
+                                    <p onClick={handleNewUser} className='link text-center'>Log In</p>
+                                </div>
+                                :
+                                <div>
+                                    <small>
+                                        <p className='mt-3 text-center'>Don't have an account</p>
+                                    </small>
+                                    <p onClick={handleNewUser} className='link text-center'>Sign Up</p>
+                                </div>
+                        }
                     </div>
                 </div>
             </div>
